809c474e7e1da19dd068f1ccf623a7cb54c1c4ad
ApxNord <nord-life17@yandex.ru>
2023-07-21 13:52:01 +0300

    Изменение под архитектуру MVVM

--- KeyboardTrainer/Models/StatisticModel.cs
+++ KeyboardTrainer/Models/StatisticModel.cs
@@ -8,13 +8,7 @@ using System.Threading.Tasks;
 
 namespace KeyboardTrainer
 {
-    /// <summary>
-    /// Для хранения и изменения данных по статистике прогресса
-    /// </summary>
-    /// <param name="Speed">скорость набора символов в минуту</param>
-    /// <param name="Fails">Количество допущенных ошибок</param>
-    /// <param name="Diff">Устанавливаемый максимум длины слов</param>
-    public class Statistic : INotifyPropertyChanged
+    public class StatisticModel : INotifyPropertyChanged
     {
 
         private int _speed; // скорость набора текста
@@ -64,20 +58,5 @@ namespace KeyboardTrainer
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public string GenerationRandomText()
-        {
-            int[] onlyLowerText = { 97, 123 };
-            Random random = new Random();
-            string text = string.Empty;
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < Diff; j++)
-                {
-                    text += (char)random.Next(onlyLowerText[0], onlyLowerText[1]);
-                }
-                text += " ";
-            }
-            return text;
-        }
     }
 }
